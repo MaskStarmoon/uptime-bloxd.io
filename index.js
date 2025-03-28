527fdf0316e5865c07003cde21376e8a70cf3976
MaskStarmoon <150095487+MaskStarmoon@users.noreply.github.com>
2025-03-28 01:48:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require("express");
-const { fork } = require("child_process");
+const joinWorld = require('./moon');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-let botProcess = null;
 
 // Start bot
 app.get("/start", async (req, res) => {
@@ -12,27 +10,15 @@ app.get("/start", async (req, res) => {
     return res.send("Bot sudah berjalan!");
   }
 
-  botProcess = fork("./moon.js");
   res.send("Bot berhasil dijalankan!");
 });
 
 // Stop bot
 app.get("/stop", async (req, res) => {
-  if (botProcess) {
-    botProcess.kill();
-    botProcess = null;
-    return res.send("Bot berhasil dihentikan.");
-  }
+
   res.send("Bot belum berjalan.");
 });
 
-// Ping otomatis agar server tetap online
-setInterval(() => {
-  require("axios")
-    .get("https://[namaprojekkamu].glitch.me/")
-    .then(() => console.log("Ping sukses!"))
-    .catch((err) => console.error("Ping gagal:", err));
-}, 240000); // 4 menit
 
 app.listen(PORT, () => {
   console.log(`Server berjalan di port ${PORT}`);
